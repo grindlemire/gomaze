5c18ed5a34d5c9877a5e920d78c5f628c5425daa
grindlemire <grindlemire@gmail.com>
2019-08-06 20:00:56 -0600

    clean up the drawing code

--- pkg/encode/png.go
+++ pkg/encode/png.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 	"image/png"
 	"io"
+	"math"
 	"os"
 
 	"github.com/grindlemire/gomaze/pkg/board"
@@ -29,33 +30,35 @@ func NewPNG(name string) (p PNG, err error) {
 	return p, nil
 }
 
+const cellWidth = 20
+
 // Encode encodes cells to a png
 func (p PNG) Encode(cells [][]*board.Cell) (err error) {
-	img := image.NewNRGBA(image.Rect(0, 0, 3*len(cells[0])+2, 3*len(cells)+2))
+	img := image.NewNRGBA(image.Rect(0, 0, cellWidth*len(cells[0])+2, cellWidth*len(cells)+2))
 
 	for y := 0; y < len(cells); y++ {
 		for x := 0; x < len(cells[0]); x++ {
 			cell := cells[y][x]
 
-			for i := -1; i <= 1; i++ {
-				for j := -1; j <= 1; j++ {
+			for i := -cellWidth / 2; i <= cellWidth/2; i++ {
+				for j := -cellWidth / 2; j <= cellWidth/2; j++ {
 					// calculate the pixel we are looking at
-					pX := x*3 + i + 1
-					pY := y*3 + j + 1
+					pX := x*cellWidth + i + 1
+					pY := y*cellWidth + j + 1
 
 					_, hasLeft := cell.Connections[board.Left]
 					_, hasRight := cell.Connections[board.Right]
 					_, hasUp := cell.Connections[board.Up]
 					_, hasDown := cell.Connections[board.Down]
+					var r, g, b, a uint8 = 255, 255, 255, 255
 
-					var r, g, b, a uint8 = 0, 0, 0, 255
-					// if we are not in the center or have a connection we are going to be black
-					if (i == 0 && j == 0) ||
-						(i < 0 && hasLeft && j == 0) ||
-						(i > 0 && hasRight && j == 0) ||
-						(i == 0 && hasUp && j < 0) ||
-						(i == 0 && hasDown && j > 0) {
-						r, g, b = 255, 255, 255
+					// if we are on an edge and there is no connection, make it black
+					if (i == -cellWidth/2 && !hasLeft) ||
+						(i == cellWidth/2 && !hasRight) ||
+						(j == -cellWidth/2 && !hasUp) ||
+						(j == cellWidth/2 && !hasDown) ||
+						(math.Abs(float64(i)) == cellWidth/2 && math.Abs(float64(j)) == cellWidth/2) { // This is the case you are in the corner
+						r, g, b = 0, 0, 0
 					}
 
 					img.Set(pX, pY, color.NRGBA{
